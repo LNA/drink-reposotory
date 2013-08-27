46cd5f6a936eb78589655f89e6a991b8292167db
LNA <ThisIsLaToyaNicole@gmail.com>
2013-08-27 16:31:02 -0500

    added seed to drink_app.rb

--- drink_app.rb
+++ drink_app.rb
@@ -8,7 +8,17 @@ require 'drink'
 
 Repository.register(:drink, Drinks.new)
 
-# add seed here
+class Seed
+  def self.drinks
+    @drink1 = Drink.new(:booze => "vodka", :mixer => "water", :glass => "rocks",:name => "drink1")
+    @drink2 = Drink.new(:booze => "gin", :mixer => "tonic", :glass => "tall",:name => "drink2" )
+    @drink3 = Drink.new(:booze => "ryb", :mixer => "sour", :glass => "tall",:name => "drink3")
+
+    [@drink1, @drink2, @drink3].each do |drink|
+      Repository.for(:drink).save(drink)
+    end
+  end
+end
 
 class DrinkApp < Sinatra::Application
   get '/' do
