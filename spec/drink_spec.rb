b7bfd8886344545faf2eb9faf805feac29b5e223
LNA <ThisIsLaToyaNicole@gmail.com>
2014-02-04 11:02:34 -0600

    changed before each to let

--- spec/drink_spec.rb
+++ spec/drink_spec.rb
@@ -1,22 +1,20 @@
 require 'spec_helper'
 
 describe Drink do 
-  before :each do
-    params = {:booze => 'vodka',
-              :mixer => 'water',
-              :glass => 'rocks',
-              :name => 'drink1'}
-    @drink1 = Drink.new(params)
-  end
+  let (:params) { {:booze => 'vodka',
+                   :mixer => 'water',
+                   :glass => 'rocks',
+                   :name => 'drink1'}  }
+  let (:drink1) {Drink.new(params)}
 
   it 'creates a new drink with params' do
-    @drink1.booze.should == 'vodka'
-    @drink1.mixer.should == 'water'
-    @drink1.glass.should == 'rocks'
+    drink1.booze.should == 'vodka'
+    drink1.mixer.should == 'water'
+    drink1.glass.should == 'rocks'
   end
 
   it 'updates the booze paramater' do
-    @drink1.update(:booze => 'rum')
-    @drink1.booze.should == 'rum'
+    drink1.update(:booze => 'rum')
+    drink1.booze.should == 'rum'
 	end
 end
